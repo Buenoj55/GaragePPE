2a3fdc453feba52ed22257798ec259278562d7bb
PierreMr <mauerpierre@gmail.com>
2017-01-14 17:07:41 +0100

    Finalisation Inscription et début Reservation

--- PPE1/Connexion/InscriptionEntreprise.php
+++ PPE1/Connexion/InscriptionEntreprise.php
@@ -42,7 +42,7 @@
                             </div>
                             <div class="form-group">
                                 <label>Activité *</label>
-                                <input type="ext" name="activite_Entreprise" class="form-control" required="required">
+                                <input type="text" name="activite_Entreprise" class="form-control" required="required">
                             </div>
                             <div class="form-group">
                                 <label>Téléphone *</label>
